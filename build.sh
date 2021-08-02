3e158bf71e1ad5c5a32f111d09118905eedede4a
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2021-08-02 18:47:02 +0000

    Pass along root passwords, and slave credentials, defaulting to master credentials, as parameters

--- build.sh
+++ build.sh
@@ -2,23 +2,41 @@
 
 # d: database name
 # n: number of slaves
-while getopts d:n:u:p: flag
+while getopts R:r:d:n:U:P:u:p: flag
 do
     case "${flag}" in
+        R) database_root_password=${OPTARG};;
+        r) slave_root_password=${OPTARG};;
         d) database_name=${OPTARG};;
         n) number_of_slaves=${OPTARG};;
-        u) database_user=${OPTARG};;
-        p) database_password=${OPTARG};;
+        U) master_database_user=${OPTARG};;
+        P) master_database_password=${OPTARG};;
+        u) slave_database_user=${OPTARG};;
+        p) slave_database_password=${OPTARG};;
     esac
 done
 
-if [[ "$database_name" == "" ]] || [[ "$number_of_slaves" == "" ]] || [[ "$database_user" == "" ]] || [[ "$database_password" == "" ]]
+if [[ "$database_root_password" == "" ]] || [[ "$database_name" == "" ]] || [[ "$number_of_slaves" == "" ]] || [[ "$master_database_user" == "" ]] || [[ "$master_database_password" == "" ]]
 then
-	echo "Please specify a database name (-d) and the number of slave instances (-n) and also database user and password (-u, -p)"
+	echo "Please specify a database root password (-R), a database name (-d) and the number of slave instances (-n) and also database user and password for the master node (-U, -P)"
 	exit
 fi
 
-./dynamic_docker_compose.py -d $database_name -n $number_of_slaves -u $database_user -p $database_password
+# Make slave parameters default to master values
+if [[ "$slave_root_password" == "" ]]
+then
+	slave_root_password=$database_root_password
+fi
+if [[ "$slave_database_user" == "" ]]
+then
+	slave_database_user=$master_database_user
+fi
+if [[ "$slave_database_password" == "" ]]
+then
+	slave_database_password=$master_database_password
+fi
+
+./dynamic_docker_compose.py -R $database_root_password -r $slave_root_password -d $database_name -n $number_of_slaves -U $master_database_user -P $master_database_password -u $slave_database_user -p $slave_database_password
 
 docker-compose down
 rm -rf ./master/data/*
@@ -26,19 +44,18 @@ rm -rf ./slave/data/*
 docker-compose build
 docker-compose up -d
 
-until docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+until docker exec mysql_master sh -c "export MYSQL_PWD=$database_root_password; mysql -u root -e \";\""
 do
     echo "Waiting for mysql_master database connection..."
     sleep 4
 done
 
-priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; FLUSH PRIVILEGES;'
-docker exec mysql_master sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
+priv_stmt="GRANT REPLICATION SLAVE ON *.* TO \"$slave_database_user\"@\"%\" IDENTIFIED BY \"$slave_database_password\"; FLUSH PRIVILEGES;"
+docker exec mysql_master sh -c "export MYSQL_PWD=$database_root_password; mysql -u root -e '$priv_stmt'"
 
 for((i=1; i <= $number_of_slaves; i++))
-# for i in {1..$nr_of_slaves}
 do
-	until docker-compose exec mysql_slave$i sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+	until docker-compose exec mysql_slave$i sh -c "export MYSQL_PWD=$slave_root_password; mysql -u root -e \";\""
 	do
 	    echo "Waiting for mysql_slave$i database connection..."
 	    sleep 4
@@ -48,15 +65,15 @@ do
 	    docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
 	}
 
-	MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
+	MS_STATUS=`docker exec mysql_master sh -c "export MYSQL_PWD=$database_root_password; mysql -u root -e \"SHOW MASTER STATUS\""`
 	CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 	CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-	start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-	start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
+	start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='$slave_database_user',MASTER_PASSWORD='$slave_database_password',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+	start_slave_cmd="export MYSQL_PWD=$slave_root_password; mysql -u root -e \""
 	start_slave_cmd+="$start_slave_stmt"
 	start_slave_cmd+='"'
 	docker exec mysql_slave$i sh -c "$start_slave_cmd"
 
-	docker exec mysql_slave$i sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+	docker exec mysql_slave$i sh -c "export MYSQL_PWD=$slave_root_password; mysql -u root -e 'SHOW SLAVE STATUS \G'"
 done
--- dynamic_docker_compose.py
+++ dynamic_docker_compose.py
@@ -50,7 +50,7 @@ def write_docker_compose_file(nr_of_slaves=1):
     fh.write(foot)
     fh.close()
 
-def prep_slave_dir(slave_nr, database_name):
+def prep_slave_dir(slave_nr, root_password, database_name, database_user, database_password):
     slave_nr_str = str(slave_nr)
     slave_dir = "./slave"+slave_nr_str
     os.system("cp -r ./slave " + slave_dir)
@@ -73,7 +73,10 @@ def prep_slave_dir(slave_nr, database_name):
     os.system("rm " + slave_dir+"/mysql_slave.env.j2")
     slave_env_fh = open(slave_dir+"/mysql_slave.env", "w")
     slave_data = {
-            "database_name": database_name
+            "root_password": root_password,
+            "database_name": database_name,
+            "database_user": database_user,
+            "database_password": database_password
     }
     j2_tpl = Template(slave_env_tpl)
     slave_env_str = j2_tpl.render(slave_data)
@@ -81,12 +84,12 @@ def prep_slave_dir(slave_nr, database_name):
     slave_env_fh.close()
 
 
-def do_all_dirs(nr_of_slaves, database_name):
+def do_all_dirs(nr_of_slaves, root_password, database_name, database_user, database_password):
     os.system("rm -r ./slave[0-9]")
     for i in range(1, nr_of_slaves + 1):
-        prep_slave_dir(i, database_name)
+        prep_slave_dir(i, root_password, database_name, database_user, database_password)
 
-def do_master(database_name, database_user, database_password):
+def do_master(database_root_password, database_name, database_user, database_password):
     os.system("rm -r ./master")
     os.system("cp -r ./master_tpl ./master")
     with open("./master_tpl/conf/mysql.conf.cnf.j2", "r") as template_conf_fh:
@@ -106,6 +109,7 @@ def do_master(database_name, database_user, database_password):
     os.system("rm ./master/mysql_master.env.j2")
     env_fh = open("./master/mysql_master.env", "w")
     env_data = {
+            "database_root_password": database_root_password,
             "database_name": database_name,
             "database_user": database_user,
             "database_password": database_password
@@ -122,25 +126,43 @@ def do_master(database_name, database_user, database_password):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-R", "--database-root-password" )
+    parser.add_argument("-r", "--slave-root-password" )
     parser.add_argument("-n", "--number-of-slaves" )
     parser.add_argument("-d", "--database-name" )
-    parser.add_argument("-u", "--database-user" )
-    parser.add_argument("-p", "--database-password" )
+    parser.add_argument("-U", "--master-database-user" )
+    parser.add_argument("-P", "--master-database-password" )
+    parser.add_argument("-u", "--slave-database-user" )
+    parser.add_argument("-p", "--slave-database-password" )
 
     args = parser.parse_args()
 
-    if(args.number_of_slaves is None or args.database_name is None \
-            or args.database_user is None or args.database_password is None):
+    if(args.database_root_password is None or args.number_of_slaves is None \
+            or args.database_name is None \
+            or args.master_database_user is None or args.master_database_password is None):
         exit("Please specify the number of slave instances and a database name!")
 
     number_of_slaves = int(args.number_of_slaves)
+    database_root_password = args.database_root_password
+    slave_root_password = args.slave_root_password
     database_name = args.database_name
-    database_user = args.database_user
-    database_password = args.database_password
+    master_database_user = args.master_database_user
+    master_database_password = args.master_database_password
+    slave_database_user = args.slave_database_user
+    slave_database_password = args.slave_database_password
+
+    # Making slave values default to master values.
+    if slave_root_password == "" or slave_root_password is None:
+        slave_root_password = database_root_password
+    if slave_database_user == "" or slave_database_user is None:
+        slave_database_user = master_database_user
+    if slave_database_password == "" or slave_database_password is None:
+        slave_database_password = master_database_password
 
     write_docker_compose_file(number_of_slaves)
-    do_master(database_name, database_user, database_password)
-    do_all_dirs(number_of_slaves, database_name)
+    do_master(database_root_password, database_name, master_database_user, master_database_password)
+    do_all_dirs(number_of_slaves, slave_root_password, database_name, \
+            slave_database_user, slave_database_password)
 
 if __name__ == "__main__":
     main()
